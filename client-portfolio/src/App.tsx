f9dceb5faa728ebeb407038f1696cc81db1bbb55
galoisz <galzfira@gmail.com>
2024-11-09 19:50:07 +0200

    add time range component

--- client-portfolio/src/App.tsx
+++ client-portfolio/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./trial/pages/Home";
 import About from "./trial/pages/About";
 import ContactForm from "./trial/components/ContactForm";
 import StockForm from "./trial/components/StockForm";
+import TimeRangeForm from "./trial/components/TimeRangeForm";
 
 const App: React.FC = () => (
   <>
@@ -12,6 +13,7 @@ const App: React.FC = () => (
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<ContactForm />} />
       <Route path="/stock" element={<StockForm />} />
+      <Route path="/time-range" element={<TimeRangeForm />} />
     </Routes>
   </>
 );
